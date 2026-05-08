62b1131ee73c71fe4b31dbbc3dbe232bb589ffc1
vlad <vlad@gate.me>
2026-05-08 15:34:56 +0800

    文件名处理

--- Sources/ReownAppKit/Models/Helpers/Bundle+extension.swift
+++ Sources/ReownAppKit/Models/Helpers/Bundle+extension.swift
@@ -6,8 +6,15 @@ public extension Foundation.Bundle {
 
     static var coreModule: Bundle {
         let bundle = Bundle(for: CocoapodsBundle.self)
-        let frameworkBundlePath = bundle.path(forResource: "Web3Modal", ofType: "bundle")!
-        return Bundle(path: frameworkBundlePath) ?? bundle
+        if let frameworkBundlePath = bundle.path(forResource: "ReownAppKit", ofType: "bundle"),
+           let resourceBundle = Bundle(path: frameworkBundlePath) {
+            return resourceBundle
+        }
+        if let mainPath = Bundle.main.path(forResource: "ReownAppKit", ofType: "bundle"),
+           let resourceBundle = Bundle(path: mainPath) {
+            return resourceBundle
+        }
+        return bundle
     }
 }
 #else
--- Sources/ReownAppKitUI/Helpers/Bundle.swift
+++ Sources/ReownAppKitUI/Helpers/Bundle.swift
@@ -7,8 +7,15 @@ public extension Foundation.Bundle {
     
     static var module: Bundle {
         let bundle = Bundle(for: CocoapodsBundle.self)
-        let frameworkBundlePath = bundle.path(forResource: "Web3ModalUI", ofType: "bundle")!
-        return Bundle(path: frameworkBundlePath) ?? bundle
+        if let frameworkBundlePath = bundle.path(forResource: "ReownAppKitUI", ofType: "bundle"),
+           let resourceBundle = Bundle(path: frameworkBundlePath) {
+            return resourceBundle
+        }
+        if let mainPath = Bundle.main.path(forResource: "ReownAppKitUI", ofType: "bundle"),
+           let resourceBundle = Bundle(path: mainPath) {
+            return resourceBundle
+        }
+        return bundle
     }
 }
 
